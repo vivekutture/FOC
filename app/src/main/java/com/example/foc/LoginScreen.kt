27fad8f327604e579525b8ff1dee6828bec9c915
Vivek Utture <uttureviveks@gmail.com>
2024-08-05 21:53:48 +0530

    implementing LoginScreen

--- app/src/main/java/com/example/foc/LoginScreen.kt
+++ app/src/main/java/com/example/foc/LoginScreen.kt
@@ -1,22 +1,40 @@
 package com.example.foc
 
+import android.widget.Toast
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.material3.OutlinedTextField
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Visibility
+import androidx.compose.material.icons.filled.VisibilityOff
+import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -24,19 +42,74 @@ import androidx.compose.ui.unit.sp
 @Preview(showBackground = true)
 @Composable
 fun LoginScreen(){
+    val loginId = remember {
+        mutableStateOf("")
+    }
+    val password = remember {
+        mutableStateOf("")
+    }
+    val passwordVisible = remember { mutableStateOf(false) }
+
+    val context= LocalContext.current
     Column(verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally, modifier = Modifier.fillMaxSize()){
-        Image(painter = painterResource(id = R.drawable.foc), contentDescription ="FOC Image", modifier = Modifier.width(350.dp),contentScale = ContentScale.FillWidth)
-        Spacer(modifier = Modifier.padding(20.dp))
-        Row(horizontalArrangement = Arrangement.Center, verticalAlignment = Alignment.CenterVertically){
-            Text("LoginId")
-            Spacer(modifier = Modifier.padding(15.dp))
-            OutlinedTextField(value = "", onValueChange = {})
+        Image(painter = painterResource(id = R.drawable.foc), contentDescription ="FOC Image", modifier = Modifier
+            .width(350.dp)
+            .padding(bottom = 40.dp),contentScale = ContentScale.FillWidth)
+        Text("Login", modifier = Modifier.padding(bottom = 40.dp), fontSize = 25.sp, fontWeight = FontWeight.Bold)
+        Row(horizontalArrangement = Arrangement.Center, verticalAlignment = Alignment.CenterVertically,modifier = Modifier.padding(bottom = 40.dp)){
+            Text("Login ID", fontSize = 18.sp, fontWeight = FontWeight.SemiBold, modifier = Modifier.width(100.dp), textAlign = TextAlign.Center)
+            TextField(value = loginId.value, onValueChange = {loginId.value=it}, modifier = Modifier
+                .width(250.dp)
+                .border(
+                    1.dp, Color.Black,
+                    RoundedCornerShape(5.dp)
+                ),  shape = RoundedCornerShape(5.dp),singleLine = true,                keyboardOptions = KeyboardOptions.Default.copy(imeAction = ImeAction.Next),colors = TextFieldDefaults.colors(
+                focusedIndicatorColor = Color.Transparent,
+                unfocusedIndicatorColor = Color.Transparent,
+                disabledIndicatorColor = Color.Transparent,
+                unfocusedContainerColor = Color.Transparent,
+                disabledContainerColor = Color.Transparent,
+            )
+            )
         }
-        Spacer(modifier = Modifier.padding(20.dp))
-        Row(horizontalArrangement = Arrangement.Center, verticalAlignment = Alignment.CenterVertically){
-            Text("Password")
-            Spacer(modifier = Modifier.padding(6.dp))
-            OutlinedTextField(value = "", onValueChange = {})
+        Row(horizontalArrangement = Arrangement.Center, verticalAlignment = Alignment.CenterVertically, modifier = Modifier.padding(bottom = 40.dp)){
+            Text("Password",fontSize = 18.sp, fontWeight = FontWeight.SemiBold,modifier = Modifier.width(100.dp), textAlign = TextAlign.Center)
+            TextField(
+                value = password.value,
+                onValueChange = { password.value = it },
+                modifier = Modifier
+                    .width(250.dp)
+                    .border(
+                        1.dp, Color.Black,
+                        RoundedCornerShape(5.dp)
+                    ),
+                shape = RoundedCornerShape(5.dp),
+                singleLine = true,
+                visualTransformation = if (passwordVisible.value) VisualTransformation.None else PasswordVisualTransformation(),
+                keyboardOptions = KeyboardOptions.Default.copy(imeAction = ImeAction.Done),
+                colors = TextFieldDefaults.colors(
+                    focusedIndicatorColor = Color.Transparent,
+                    unfocusedIndicatorColor = Color.Transparent,
+                    disabledIndicatorColor = Color.Transparent,
+                    unfocusedContainerColor = Color.Transparent,
+                    disabledContainerColor = Color.Transparent,
+                ),
+                trailingIcon = {
+                    val image = if (passwordVisible.value)
+                        Icons.Filled.Visibility
+                    else Icons.Filled.VisibilityOff
+
+                    // Please provide localized description for accessibility services
+                    val description = if (passwordVisible.value) "Hide password" else "Show password"
+
+                    IconButton(onClick = {passwordVisible.value = !passwordVisible.value}){
+                        Icon(imageVector  = image, description)
+                    }
+                }
+            )
+        }
+        Button(onClick = { Toast.makeText(context,"Login Successful",Toast.LENGTH_SHORT).show()}, modifier = Modifier.size(150.dp,50.dp), enabled = !(loginId.value.isEmpty() || password.value.isEmpty())) {
+            Text(text = "Login", fontSize = 20.sp)
         }
     }
 }
--- app/src/main/java/com/example/foc/MainActivity.kt
+++ app/src/main/java/com/example/foc/MainActivity.kt
@@ -4,6 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Scaffold
+import androidx.compose.ui.Modifier
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,6 +14,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             LoginScreen()
+
         }
     }
 }
