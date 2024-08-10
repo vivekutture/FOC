0cf2dbc42d4b3d7a686761b4b732c871e0e69292
Vivek Utture <uttureviveks@gmail.com>
2024-08-11 01:03:34 +0530

    implementing LoginScreen

--- app/src/main/java/com/example/foc/LoginScreen.kt
+++ app/src/main/java/com/example/foc/LoginScreen.kt
@@ -1,47 +1,29 @@
 package com.example.foc
 
 import android.widget.Toast
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.*
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Visibility
-import androidx.compose.material.icons.filled.VisibilityOff
-import androidx.compose.material3.Button
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
-import androidx.compose.material3.TextFieldDefaults
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
+import androidx.compose.material.icons.filled.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.*
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.input.ImeAction
-import androidx.compose.ui.text.input.PasswordVisualTransformation
-import androidx.compose.ui.text.input.VisualTransformation
-import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.input.*
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
+import androidx.compose.ui.unit.*
 
 @Preview(showBackground = true)
 @Composable
-fun LoginScreen(){
+fun LoginScreen(modifier: Modifier = Modifier) {
+    val scrollState = rememberScrollState()
     val loginId = remember {
         mutableStateOf("")
     }
@@ -50,65 +32,93 @@ fun LoginScreen(){
     }
     val passwordVisible = remember { mutableStateOf(false) }
 
-    val context= LocalContext.current
-    Column(verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally, modifier = Modifier.fillMaxSize()){
-        Image(painter = painterResource(id = R.drawable.foc), contentDescription ="FOC Image", modifier = Modifier
-            .width(350.dp)
-            .padding(bottom = 40.dp),contentScale = ContentScale.FillWidth)
-        Text("Login", modifier = Modifier.padding(bottom = 40.dp), fontSize = 25.sp, fontWeight = FontWeight.Bold)
-        Row(horizontalArrangement = Arrangement.Center, verticalAlignment = Alignment.CenterVertically,modifier = Modifier.padding(bottom = 40.dp)){
-            Text("Login ID", fontSize = 18.sp, fontWeight = FontWeight.SemiBold, modifier = Modifier.width(100.dp), textAlign = TextAlign.Center)
-            TextField(value = loginId.value.trim(), onValueChange = {loginId.value=it.trim()}, modifier = Modifier
-                .width(250.dp)
-                .border(
-                    1.dp, Color.Black,
-                    RoundedCornerShape(5.dp)
-                ),  shape = RoundedCornerShape(5.dp),singleLine = true,                keyboardOptions = KeyboardOptions.Default.copy(imeAction = ImeAction.Next),colors = TextFieldDefaults.colors(
-                focusedIndicatorColor = Color.Transparent,
-                unfocusedIndicatorColor = Color.Transparent,
-                disabledIndicatorColor = Color.Transparent,
-                unfocusedContainerColor = Color.Transparent,
-                disabledContainerColor = Color.Transparent,
-            )
+    val context = LocalContext.current
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier
+            .fillMaxSize()
+            .verticalScroll(scrollState)
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.foc),
+            contentDescription = "FOC Image",
+            modifier = Modifier
+                .width(350.dp)
+                .padding(bottom = 40.dp),
+            contentScale = ContentScale.FillWidth
+        )
+        Text(
+            "Login",
+            modifier = Modifier.padding(bottom = 40.dp),
+            fontSize = 25.sp,
+            fontWeight = FontWeight.Bold
+        )
+        Row(
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.padding(bottom = 40.dp)
+        ) {
+
+            OutlinedTextField(
+                value = loginId.value.trim(),
+                onValueChange = { loginId.value = it.trim() },
+                modifier = Modifier
+                    .width(350.dp),
+                singleLine = true,
+                keyboardOptions = KeyboardOptions.Default.copy(imeAction = ImeAction.Next),
+                label = { Text(text = "Login ID", fontSize = 15.sp) }
             )
         }
-        Row(horizontalArrangement = Arrangement.Center, verticalAlignment = Alignment.CenterVertically, modifier = Modifier.padding(bottom = 40.dp)){
-            Text("Password",fontSize = 18.sp, fontWeight = FontWeight.SemiBold,modifier = Modifier.width(100.dp), textAlign = TextAlign.Center)
-            TextField(
+        Row(
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.padding(bottom = 40.dp)
+        ) {
+
+            OutlinedTextField(
                 value = password.value.trim(),
                 onValueChange = { password.value = it.trim() },
                 modifier = Modifier
-                    .width(250.dp)
-                    .border(
-                        1.dp, Color.Black,
-                        RoundedCornerShape(5.dp)
-                    ),
-                shape = RoundedCornerShape(5.dp),
+                    .width(350.dp),
                 singleLine = true,
                 visualTransformation = if (passwordVisible.value) VisualTransformation.None else PasswordVisualTransformation(),
                 keyboardOptions = KeyboardOptions.Default.copy(imeAction = ImeAction.Done),
-                colors = TextFieldDefaults.colors(
-                    focusedIndicatorColor = Color.Transparent,
-                    unfocusedIndicatorColor = Color.Transparent,
-                    disabledIndicatorColor = Color.Transparent,
-                    unfocusedContainerColor = Color.Transparent,
-                    disabledContainerColor = Color.Transparent,
-                ),
                 trailingIcon = {
                     val image = if (passwordVisible.value)
                         Icons.Filled.Visibility
                     else Icons.Filled.VisibilityOff
+                    val description =
+                        if (passwordVisible.value) "Hide password" else "Show password"
 
-                    // Please provide localized description for accessibility services
-                    val description = if (passwordVisible.value) "Hide password" else "Show password"
-
-                    IconButton(onClick = {passwordVisible.value = !passwordVisible.value}){
-                        Icon(imageVector  = image, description)
+                    IconButton(onClick = { passwordVisible.value = !passwordVisible.value }) {
+                        Icon(imageVector = image, description)
                     }
-                }
+                },
+                label = { Text(text = "Password", fontSize = 15.sp) }
             )
         }
-        Button(onClick = { Toast.makeText(context,"Login Successful",Toast.LENGTH_SHORT).show()}, modifier = Modifier.size(150.dp,50.dp), enabled = !(loginId.value.trim().isEmpty() || password.value.trim().isEmpty())) {
+        Text(
+            text = "Forgot Password",
+            fontSize = 16.sp,
+            modifier = Modifier
+                .padding(bottom = 40.dp)
+                .clickable(
+                    true,
+                    onClick = {
+                        Toast
+                            .makeText(context, "Forgot Password", Toast.LENGTH_SHORT)
+                            .show()
+                    }, role = Role.Button
+                ),
+            color = Color.Blue,
+            textDecoration = TextDecoration.Underline
+        )
+        Button(
+            onClick = { Toast.makeText(context, "Login Successful", Toast.LENGTH_SHORT).show() },
+            modifier = Modifier.size(150.dp, 50.dp),
+            enabled = !(loginId.value.trim().isEmpty() || password.value.trim().isEmpty())
+        ) {
             Text(text = "Login", fontSize = 20.sp)
         }
     }
--- app/src/main/java/com/example/foc/MainActivity.kt
+++ app/src/main/java/com/example/foc/MainActivity.kt
@@ -1,20 +1,65 @@
 package com.example.foc
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
+import androidx.activity.*
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
 
+
+@ExperimentalMaterial3Api
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            LoginScreen()
+            val scrollBehavior = TopAppBarDefaults.pinnedScrollBehavior(rememberTopAppBarState())
 
+            Scaffold(
+                modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+                topBar = {
+                    TopAppBar(
+                        colors = TopAppBarDefaults.topAppBarColors(
+                            containerColor = MaterialTheme.colorScheme.primaryContainer,
+                            titleContentColor = MaterialTheme.colorScheme.primary,
+                        ),
+                        title = {
+                            Text(
+                                "FOC",
+                                maxLines = 1,
+                                overflow = TextOverflow.Ellipsis
+                            )
+                        },
+                        navigationIcon = {
+                            IconButton(onClick = { /* do something */ }) {
+                                Icon(
+                                    imageVector = Icons.Filled.Menu,
+                                    contentDescription = "Localized description"
+                                )
+                            }
+                        },
+                        actions = {
+                            IconButton(onClick = { /* do something */ }) {
+                                Icon(
+                                    imageVector = Icons.Filled.Menu,
+                                    contentDescription = "Localized description"
+                                )
+                            }
+                        },
+                        scrollBehavior = scrollBehavior,
+                    )
+                },
+                content = { innerPadding ->
+                    LoginScreen(modifier = Modifier.padding(innerPadding))
+                }
+            )
         }
     }
 }
